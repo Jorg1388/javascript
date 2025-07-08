454174b359600ab0597b46eb401436e8fd412847
Jorge <jgomez@scl.edu.gt>
2025-07-08 10:22:16 -0600

    Conceptos de js

---
+++ operadoresDeComparacion.js
@@ -0,0 +1,34 @@
+console.log("[Operados de comparacion]");
+console.log("-----------------------------");
+
+let num3 = 5;
+let num4 = 10;
+
+//Operadores de comparacion
+let estado1 = num3 == num4;
+console.log("Estado 1: ", estado1);
+
+let estado2 = num3 <= num4;
+console.log("Estado 2: ", estado2);
+
+let estado3 = num3 >= num4;
+console.log("Estado 3: ", estado3);
+
+let estado4 = num3 < num4;
+console.log("Estado 4: ", estado4);
+
+let estado5 = num3 > num4;
+console.log("Estado 5: ", estado5);
+
+let estado6 = num3 != num4;
+console.log("Estado 6: ", estado6);
+
+//Operadores logicos
+let estado7 = estado1 && estado2;
+console.log("Estado 7: ", estado7);
+
+let estado8 = estado1 || estado2;
+console.log("Estado 8: ", estado8);
+
+let estado9 = !estado1;
+console.log("Estado 9: ", estado9);
---
+++ operadoresMatematicos.js
@@ -0,0 +1,21 @@
+console.log("[Operadores Matemáticos]");
+console.log("-----------------------------");
+
+let edad2 = 16;
+let fecha = 2025;
+
+let fechaN = fecha - edad2;
+console.log("Resultado", fechaN);
+
+//
+let num1= 20;
+let num2= 5;
+
+
+let suma = num1 + num2;
+let multiplicacion = num1 * num2;
+let division = num1 / num2;
+
+console.log("Resultado", suma);
+console.log("Resultado", multiplicacion);
+console.log("Resultado", division);
